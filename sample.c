a9fa0527af15820156ff70b3f1c3413b392acb33
GabeRoque97 <gdude2142@gmail.com>
2019-03-07 19:39:53 -0800

    first commit

--- sample.c
+++ sample.c
@@ -1,8 +1,29 @@
-main()
+#include <err.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int
+main(int argc, char**argv)
 { 
-	
-	while(1){
-	  getLine()
+	char *line = NULL;
+	size_t linesize = 0;
+	size_t linelen;
+
+	while((linelen = getline(&line, &linesize, stdin)) != -1){
+		if(strncmp("exit", line, 4) == 0) {
+			exit(0);
+		}
+		
+		fwrite(line, linelen, 1, stdout):
+		//fork
+		//if child, exec
+		//if parent, wait
+	  
 	  }
+	free(line);
+	if(ferror(stdin)) {
+		err(1,"getline");
+	}
 
 }
