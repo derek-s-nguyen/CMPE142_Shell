2740a45c65410e9a7fb1d460b0ef1ba76a1ffd66
GabeRoque97 <gdude2142@gmail.com>
2019-03-08 19:33:26 -0800

    added compiler and run code to forknife. Sample.c runs when calling forknife in terminal now

--- forknife.sh
+++ forknife.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
 echo "Hello World!"
+gcc sample.c -o sample
+./sample
--- sample.c
+++ sample.c
@@ -10,18 +10,44 @@ main(int argc, char**argv)
 	char *line = NULL;
 	size_t linesize = 0;
 	size_t linelen;
-
+	char cwd[1024];
+	getcwd(cwd, sizeof(cwd));
+	printf("\nDir: %s",cwd);	
+	printf("\nHello! Welcome to Forknife! You're about to have a dabbing good time!\n");
+	
 	while((linelen = getline(&line, &linesize, stdin)) != -1){
 		if(strncmp("exit", line, 4) == 0) {
+			printf("Goodbye :^) \n");
 			exit(0);
 		}
 		
-		fwrite(line, linelen, 1, stdout):
-		//fork
+		fwrite(line, linelen, 1, stdout);
+		
+		/*//forking child
+		pid_t pid = fork();
+
+		if(pid == -1){//forking failed
+			printf("\nForking child failed");
+			return 0;
+		} 
+		else if(pid == 0) {//processing typed in command 
+			if (execvp(argv[0], argv) < 0){
+				printf("\nCould not execute command");
+			}
+		exit(0);
+		}
+		else {//waiting for child to terminate 
+			wait(NULL);
+			return 0;
+		}
+
+
+
 		//if child, exec
 		//if parent, wait
-	  
+	  */
 	  }
+
 	free(line);
 	if(ferror(stdin)) {
 		err(1,"getline");
