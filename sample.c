ed675088aab3e0307b272d8de6816526b7ac9d3c
Derek Nguyen <dngsbc@gmail.com>
2019-03-16 00:49:17 -0700

    just adding my part

--- sample.c
+++ sample.c
@@ -34,6 +34,62 @@ perror("forknife");
 return 1;
 }
 
+//this is a built-in command to exit by returning zero
+int forknife_exit(char **args)
+{
+	return 0;
+}
+//function that will launch a program and wait for it to terminate
+int forknife_launch(char **args)
+{
+	pid_t pid;
+	int status;
+
+	pid = fork();
+	if (pid == 0) {
+		//child process
+		if(execvp(args[0], args) == -1) {
+			perror("forknife");
+		}
+		exit(EXIT_FAILURE);
+	} else if (pid < 0) {
+		//error forking
+		perror("forknife");
+	} else {
+		//parent process
+		do {
+			waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+	return 1;
+}
+int forknife_num_builtins() {
+	return sizeof(builtin_str) / sizeof(char *);
+}
+//execute built-in command or launch program
+int forknife_execute(char **args)
+{
+	int i;
+	if (args[0] == NULL) {
+		//an empty command was entered
+		return 1;
+	}
+	for(i = 0; i < forknife_num_builtins(); i++) {
+		if(strcmp(args[0], builtin_str[i]) == 0) {
+			return (*builtin_func[i]))(args);
+		}
+	}
+	return forknife_launch(args);
+}
+#define FORKNIFE_RL_BUFSIZE 1024
+char *forknife_read_line(void)
+{
+	char *line = NULL;
+	ssize_t bufsize = 0;//have getline allocate a buffer for us
+	getline(&line, &bufsize, stdin);
+	return line;
+}
+
 int main(int argc, char**argv)
 { 
 	char *line = NULL;
